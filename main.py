11e789e9a94662568b3c353565ee4eba1702b47d
RigalHD <103758149+RigalHD@users.noreply.github.com>
2024-03-08 19:31:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import keyboards
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command, CommandObject, CommandStart
 from aiogram.types import Message
-from cfgs import TOKEN
-# from aiogram.types.user import User
 
+from cfgs import TOKEN
 
 bot = Bot(TOKEN, parse_mode="HTML")
 dp = Dispatcher()
@@ -15,14 +14,12 @@ dp = Dispatcher()
 async def start(message: Message):
     await message.answer(f"Приветствую, <b>{message.from_user.first_name}</b>", reply_markup=keyboards.main_keyboard)
     await message.answer(f"{message.from_user.id, message.from_user.id}")
-    
-# @dp.message(Command(commands=["VK"]))
-# async def process_start_command(message: Message):
-#     await message.reply('<a href="https://vk.com/meljnichenko">VK</a>',parse_mode="HTML")
+
 
 @dp.message(Command(commands=["download", "скачать", "файл"]))
 async def file_download(message: Message, command: CommandObject):
     pass
+
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
