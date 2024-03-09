071a0924c876ba0fc2cabcee63d4d5938707356f
RigalHD <profoundromeo@gmail.com>
2024-03-09 16:07:37 +0300

    file division added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
 import keyboards
 
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, F, Dispatcher
 from aiogram.filters import Command, CommandObject, CommandStart
 from aiogram.types import Message
 
 from cfgs import TOKEN
+from main_commands import user_commands
+
 
 bot = Bot(TOKEN, parse_mode="HTML")
 dp = Dispatcher()
@@ -19,19 +21,15 @@ async def start(message: Message):
     await message.answer(f"Ваш айди -> {message.from_user.id}")
 
 
-@dp.message(Command(commands=["test_command"]))
-async def command_test(message: Message, command: CommandObject):
-    pass
-
-
-@dp.message(F.text.lower() == "калькулятор")
-async def calculator(message: Message):
-    await message.answer(f"1 + 1 = 2")
-
 
 async def main():
+    dp.include_routers(
+        user_commands.router
+    )
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
     print("BOT IS READY TO WORK")
 
 
---
+++ main_commands/user_commands.py
@@ -0,0 +1,18 @@
+import asyncio
+import keyboards
+
+from aiogram import Bot, Router, F
+from aiogram.filters import Command, CommandObject
+from aiogram.types import Message
+
+router = Router()
+from main import bot
+
+@router.message(F.text.lower() == "test_bot")
+async def command_test(message: Message):
+    print("ok")
+
+
+@router.message(F.text.lower() == "калькулятор")
+async def calculator(message: Message):
+    await message.answer(f"1 + 1 = 2")
