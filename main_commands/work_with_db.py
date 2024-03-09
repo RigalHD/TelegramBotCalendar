6ba0c6bdb62f3747e2c1644a39cb61fb05b780f7
RigalHD <profoundromeo@gmail.com>
2024-03-09 17:46:43 +0300

    db_add_meet:
    still in development,
    now you can add new meetings of books club to it

--- main_commands/work_with_db.py
+++ main_commands/work_with_db.py
@@ -10,12 +10,11 @@ router = Router()
 async def db_create(message: Message, command: CommandObject):
     '''Создает базу данных'''
     if message.from_user.id != 997987348:
-        await message.answer(message="Отказано в доступе")
+        await message.answer(text="Отказано в доступе")
     with sqlite3.connect("db.db") as db:
         cursor = db.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS schedule (
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       name CHAR,
                        description TEXT,
                        day DATE,
                        time TIME
@@ -24,7 +23,20 @@ async def db_create(message: Message, command: CommandObject):
 
 @router.message(Command("db_add_meet"))
 async def db_add_meet(message: Message, command: CommandObject):
+    '''Добавление в расписание встреч новую'''
     if message.from_user.id != 997987348:
-        await message.answer(message="Отказано в доступе")
+        await message.answer(text="Отказано в доступе")
+    if not command.args:
+        await message.answer(
+            text=\
+            "Добавьте встречу вот так: \n/db_add_meet <Дата (пример: 01.01.2024)> <Время> (пример: 12:00)", 
+            # Добавить потом: <Описание встречи (вкратце о том, что на ней будет)>
+            parse_mode=None)
+        return
+    print(command.args)
     with sqlite3.connect("db.db") as db:
         cursor = db.cursor()
+        cursor.execute("""INSERT INTO schedule (
+                       description, day, time)
+                       VALUES (?, ?, ?)""", ("-", *command.args.split()))
+        print(cursor.execute("""SELECT * FROM schedule""").fetchall())
