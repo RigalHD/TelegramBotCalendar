62895d77525a5b49433aa32098a4e7ecf73e847e
RigalHD <profoundromeo@gmail.com>
2024-06-01 13:28:24 +0300

    removed non-inline old main keyboard

--- all_keyboards/keyboards.py
+++ all_keyboards/keyboards.py
@@ -1,24 +1,4 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from utils.database import AdminDatabase
-
-
-# def main_kb(user_id: int) -> ReplyKeyboardBuilder:
-#     items = ["Расписание", "Регистрация", "О нас", "Настройки"]
-#     AdminDatabase.renew_table()
-#     if AdminDatabase.is_admin(user_tg_id=user_id):
-#         items.append("/add_book")
-#         items.append("/view_random_books")
-        
-#     builder = ReplyKeyboardBuilder()
-
-#     for item in items:
-#         builder.button(text=item)
-    
-#     return builder.as_markup(
-#         resize_keyboard=True,
-#         input_field_placeholder="Выберите действие из меню",
-#         selective=True,
-#         )
 
 
 def book_age_rating_kb():
@@ -27,5 +7,3 @@ def book_age_rating_kb():
     for item in items:
         builder.button(text=item)
     return builder.as_markup()
-
-
